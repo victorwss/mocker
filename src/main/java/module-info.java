0fb49b8791531eeffce668bc24aa4c157d013a0f
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2021-08-23 18:15:56 -0300

    Fix for a few warnings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ import ninja.javahacker.mocker.Mocker;
  * Defines the {@link Mocker} class which permits easy and simple mocking of interfaces.
  * @see ninja.javahacker.mocker.Mocker
  */
+@SuppressWarnings({ "requires-automatic", "requires-transitive-automatic" })
 module ninja.javahacker.mocker {
     requires transitive ninja.javahacker.reifiedgeneric;
     requires transitive static lombok;
